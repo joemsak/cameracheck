12723e05ef1c66a538ac3813421ae77fd308fcbc
Joe Sak <joe@joesak.com>
2012-11-02 16:16:06 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cameracheck::Application.config.secret_token = '6b97bea8e289c557bf7e4da017d90a876eab04039d8f1774f21a496e067f316ee20fd0f3e0b787d7e86900df34a36f344b9da7742a8bcb77925c1c5b64119ae1'
