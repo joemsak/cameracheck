952b615e1ed62c51b0ea0815f43207b3379f7df1
Joe Sak <joe@joesak.com>
2012-11-03 20:23:32 -0400

    Set the default order of venues

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,9 +1,10 @@
 class Venue < ActiveRecord::Base
+  default_scope :order => :name
+
   attr_accessible :name
 
   validates :name, :presence => true
 
   include AddressLookup::Yelp
   lookup_address_by :name
-
 end
