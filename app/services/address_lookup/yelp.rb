2e3e873c1c98a4ff546b209bd11b8e1e5024191c
Joe Sak <joe@joesak.com>
2012-11-04 22:26:06 -0600

    Add yelp lib back w/o sensitive data in it

---
+++ app/services/address_lookup/yelp.rb
@@ -0,0 +1,60 @@
+module AddressLookup
+  module Yelp
+    mattr_accessor :api_key
+
+    def self.included(base)
+      base.extend ClassMethods
+    end
+
+    module ClassMethods
+      def lookup_address_by(field)
+        before_validation do
+          if address.blank? && !send(field).blank?
+            self.address = find_address_by(field)
+          end
+        end
+      end
+
+      def request(options = {})
+        options = default_options.merge(options)
+        request = ::Yelp::V1::Review::Request::Location.new(options)
+      end
+
+      private
+      def city
+        "New York"
+      end
+
+      def state
+        "NY"
+      end
+
+      def default_options
+        { :city   => city,
+          :state  => state,
+          :radius => 2,
+          :term   => nil,
+          :yws_id => Yelp.api_key }
+      end
+    end
+
+    def find_address_by(field)
+      term     = send(field)
+      request  = self.class.request(:term => term)
+      response = YELP_CLIENT.search(request)
+
+      if business = response['businesses'].first
+        build_address(business)
+      end
+    end
+
+    private
+    def build_address(business)
+      %w(address1 address2 city state zip).map do |attr|
+        value = business[attr]
+        value << "," if attr == 'city'
+        value
+      end.join(' ')
+    end
+  end
+end
