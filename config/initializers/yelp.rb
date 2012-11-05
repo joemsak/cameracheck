2f91a158058c86c7b13c40d18f267d6d4a3596f5
Joe Sak <joe@joesak.com>
2012-11-04 22:21:35 -0600

    Remove api key from code

--- config/initializers/yelp.rb
+++ config/initializers/yelp.rb
@@ -1,2 +1,6 @@
 # construct a client instance
 YELP_CLIENT = Yelp::Client.new
+
+class MustSetENVConfigYELP_WSID< Exception; end
+raise MustSetENVConfigYELP_WSID unless key = ENV['YELP_WSID']
+AddressLookup::Yelp.api_key = key
