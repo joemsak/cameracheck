b0bae167d56964d90131cee54d43c1e257c99a6f
Joe Sak <joe@joesak.com>
2012-11-06 08:44:40 -0600

    Configure origin state/city by subdomain ALMOST haha

--- config/initializers/yelp.rb
+++ config/initializers/yelp.rb
@@ -2,7 +2,11 @@ class MustSetENVConfigYELP_WSID < Exception; end
 
 raise MustSetENVConfigYELP_WSID unless key = ENV['YELP_WSID']
 
+subdomain = 'nyc'
+origins   = { 'nyc'     => { :city => 'New York', :state => 'NY' },
+              'chicago' => { :city => 'Chicago',  :state => 'IL' } }
+
 AddressLookup::Yelp.api_key        = key
-AddressLookup::Yelp.origin_city    = "New York"
-AddressLookup::Yelp.origin_state   = "NY"
+AddressLookup::Yelp.origin_city    = origins[subdomain][:city]
+AddressLookup::Yelp.origin_state   = origins[subdomain][:state]
 AddressLookup::Yelp.client         = Yelp::Client.new
