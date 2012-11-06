6e0f60415c8946c3325d1e273ca2af1cf596d80b
Joe Sak <joe@joesak.com>
2012-11-06 08:31:09 -0600

    Test all methods of adding options

--- spec/models/options_venue_spec.rb
+++ spec/models/options_venue_spec.rb
@@ -4,21 +4,20 @@ describe OptionsVenue do
   it "works" do
     option = Option.create!(:text => 'Flash OK')
 
-    venue  = Venue.new(:name => 'Hi',
-                       :options_attributes =>
-                         { '0' => {
-                             :text => 'Point & Shoot OK'
-                           },
-                           '1' => {
-                             :text => 'SLR OK'
-                           }
-                         },
-                       :option_ids => [option.id])
+    Venue.any_instance.stub(:address) { 'present' }
 
-    venue.address = 'prevent filter'
-    venue.save
+    venue  = Venue.create(:name => 'Hi',
+                          :options_attributes =>
+                            { '0' => {
+                                :text => 'Point & Shoot OK'
+                              },
+                              '1' => {
+                                :text => 'SLR OK'
+                              }
+                            },
+                          :option_ids => [option.id])
 
-    venue.should have(3).option
+    venue.should have(3).options
     Option.first.should have(1).venue
   end
 end
